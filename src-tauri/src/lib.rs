1966b1002827b8253b8b2c8632c576a9fbbcff4b
luminousky913 <itmir913@gmail.com>
2026-07-01 23:12:17 +0900

    미사용 tauri-plugin-fs 의존성 제거

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -43,7 +43,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             load_default_csv,
             read_file_bytes,
